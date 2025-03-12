67e0cce88b66fe3dccae8fadf9f797a3c6750734
Taimoor Hamza <taimoorhamza1999@gmail.com>
2025-03-12 14:45:03 +0400

    add next.js image replace with default image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,11 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import bannerImage from "../assets/background_banner.png";
 import { motion } from "framer-motion";
 import ButtonPrimary from "./utils/ButtonPrimary";
 import inter from "@/app/InterFont";
 import useScreenStore from "../../store/useScreenStore";
+import Image from "next/image";
 
 const Banner = () => {
   const isLargeScreen = useScreenStore((state) => state.isLargeScreen);
@@ -13,67 +14,70 @@ const Banner = () => {
   const isMobileLScreen = useScreenStore((state) => state.isMobileLScreen);
   return (
     <motion.div
-      className={`relative lg:w-full h-auto min-h-[95vh] lg:min-h-[100vh] lg:-mt-[0vh] flex  md:items-center  justify-between md:mt-20 mx-auto  mb-7 md:mb-10 lg:mb-20 xl:mb-0 pt-20 lg:pb-16 font-normal ${inter.className}`}
-      style={{
-        backgroundImage: `url(${bannerImage.src})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        borderRadius: "12px",
-        color: "white",
-      }}
+      className={`relative lg:w-full h-auto min-h-[95vh] lg:min-h-[100vh] lg:-mt-[0vh] flex  md:items-center  justify-between md:mt-20 mx-auto  mb-7 md:mb-10 lg:mb-20 xl:mb-0 pt-20 lg:pb-16 font-normal ${inter.className} overflow-hidden`}
     >
-      <motion.div className=" relative page-max-width mx-auto ">
-        <div className=" mx-auto w-90 lg:w-full page-max-width grid grid-cols-1 lg:grid-cols-1 gap-12 items-center py-0 ">
-          <div className="w-full">
-            <motion.div
-              layout
-              // initial={{ opacity: 0, y: 30 }}
-              // animate={{ opacity: 1, y: 0 }}
-              // transition={{ duration: 1.2, delay: 0.5 }}
-              className={`space-y-6 text-center lg:text-start  ${isMobileSScreen?"mb-[20%] pt-[30%]":isMobileMScreen?"mb-[20%] pt-[30%]":isMobileLScreen?"mb-[10%] pt-[25%]":"mb-[40%]"} lg:mb-[2%] `}
-            >
-              {/* Title */}
-              <motion.h1
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1.2, delay: 0.2 }}
-                className={`${isMobileSScreen?"text-[2.7rem] leading-[2.7rem]":"text-[3.5rem] leading-[3.7rem]"} md:text-4xl lg:text-[3.4rem] uppercase text-center   lg:leading-[3.7rem] lg:leading-auto lg:text-7xl 2xl:text-8xl font-primary font-black text-white/90`}
+      <Image
+        src={bannerImage}
+        alt="Banner Background"
+        fill
+        priority
+        quality={100}
+        className="object-cover object-center z-0"
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw"
+      />
+      <div className="relative z-10 w-full h-full rounded-xl">
+        <motion.div className=" relative page-max-width mx-auto ">
+          <div className=" mx-auto w-90 lg:w-full page-max-width grid grid-cols-1 lg:grid-cols-1 gap-12 items-center py-0 ">
+            <div className="w-full">
+              <motion.div
+                layout
+                // initial={{ opacity: 0, y: 30 }}
+                // animate={{ opacity: 1, y: 0 }}
+                // transition={{ duration: 1.2, delay: 0.5 }}
+                className={`space-y-6 text-center lg:text-start  ${isMobileSScreen?"mb-[20%] pt-[30%]":isMobileMScreen?"mb-[20%] pt-[30%]":isMobileLScreen?"mb-[10%] pt-[25%]":"mb-[40%]"} lg:mb-[2%] `}
               >
-                Meet Your
-                <br /> AI Boss
-              </motion.h1>
-              {/* Subtitle */}
-              <motion.p
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1.2, delay: 0.4 }}
-                className="text-white/90 text-base max-w-lg  text-center w-80 mx-auto lg:w-full font-light"
-              >
-                It gives you tasks, sets deadlines, and keeps you on track. It
-                might even virtually yell at you for missing deadlines.
-              </motion.p>
-            </motion.div>
+                {/* Title */}
+                <motion.h1
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1.2, delay: 0.2 }}
+                  className={`${isMobileSScreen?"text-[2.7rem] leading-[2.7rem]":"text-[3.5rem] leading-[3.7rem]"} md:text-4xl lg:text-[3.4rem] uppercase text-center   lg:leading-[3.7rem] lg:leading-auto lg:text-7xl 2xl:text-8xl font-primary font-black text-white/90`}
+                >
+                  Meet Your
+                  <br /> AI Boss
+                </motion.h1>
+                {/* Subtitle */}
+                <motion.p
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1.2, delay: 0.4 }}
+                  className="text-white/90 text-base max-w-lg  text-center w-80 mx-auto lg:w-full font-light"
+                >
+                  It gives you tasks, sets deadlines, and keeps you on track. It
+                  might even virtually yell at you for missing deadlines.
+                </motion.p>
+              </motion.div>
 
-            {/* Buttons */}
-            <motion.div
-              layout
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1.2, delay: 0.14 }}
-              className="flex flex-col lg:flex-row items-center gap-8 mt-5 justify-center "
-            >
-              <ButtonPrimary
-                // primary={true}
-                banner="true"
-                text="TRY NOW"
-                url="https://bossgpt.com/tool/v1"
-              />
-             
-            </motion.div>
+              {/* Buttons */}
+              <motion.div
+                layout
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.2, delay: 0.14 }}
+                className="flex flex-col lg:flex-row items-center gap-8 mt-5 justify-center "
+              >
+                <ButtonPrimary
+                  // primary={true}
+                  banner="true"
+                  text="TRY NOW"
+                  url="https://bossgpt.com/tool/v1"
+                />
+               
+              </motion.div>
+            </div>
           </div>
-        </div>
-      </motion.div>
+        </motion.div>
+      </div>
     </motion.div>
   );
 };
